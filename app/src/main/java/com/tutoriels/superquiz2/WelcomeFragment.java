d06071008acdb1eb8c61623d6ce6f35f6f7f146e
Amadou Sene <amadousene64@gmail.com>
2024-11-19 10:35:56 +0000

    First commit

---
+++ app/src/main/java/com/tutoriels/superquiz2/WelcomeFragment.java
@@ -0,0 +1,73 @@
+package com.tutoriels.superquiz2;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tutoriels.superquiz2.databinding.FragmentWelcomeBinding;
+
+public class WelcomeFragment extends Fragment {
+
+    private FragmentWelcomeBinding binding;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        binding.playButton.setEnabled(false);
+    }
+
+    public static WelcomeFragment newInstance(String param1, String param2) {
+        WelcomeFragment fragment = new WelcomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.usernameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                binding.playButton.setEnabled(!s.toString().isEmpty());
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                binding.playButton.setEnabled(!s.toString().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.playButton.setEnabled(!s.toString().isEmpty());
+            }
+        });
+
+        binding.playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
