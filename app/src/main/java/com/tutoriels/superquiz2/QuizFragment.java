ee87ac71fedbc10ea6394829799a926fd926f7d5
Amadou Sene <amadousene64@gmail.com>
2024-11-28 23:00:23 +0000

    Ajout du fragment Quiz

---
+++ app/src/main/java/com/tutoriels/superquiz2/QuizFragment.java
@@ -0,0 +1,33 @@
+package com.tutoriels.superquiz2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class QuizFragment extends Fragment {
+
+    public QuizFragment() {
+        // Required empty public constructor
+    }
+
+    public static QuizFragment newInstance() {
+        QuizFragment fragment = new QuizFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz, container, false);
+    }
+}
